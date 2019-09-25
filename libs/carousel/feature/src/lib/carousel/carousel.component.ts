6a7830cd5181ed5b227ec4e17acb387a4dd0b6db
Drew Thompson <drew@commongenius.com>
2019-09-25 12:23:24 -0700

    refactor(carousel): add initial navigation arrow buttons

--- libs/carousel/feature/src/lib/carousel/carousel.component.ts
+++ libs/carousel/feature/src/lib/carousel/carousel.component.ts
@@ -4,7 +4,7 @@ import {
   Input,
   OnInit
 } from '@angular/core';
-import { CarouselItem } from '@ngx-carousel/data/models';
+import { CardinalDirection, CarouselItem } from '@ngx-carousel/data/models';
 
 @Component({
   selector: 'ngx-carousel',
@@ -16,12 +16,31 @@ import { CarouselItem } from '@ngx-carousel/data/models';
 export class CarouselComponent implements OnInit {
   /** Items to display in carousel. */
   @Input() items: CarouselItem[];
+  /** Initial index of provided `items` to select. Defaults to `0` if index is out of range. */
+  @Input() initialIndex = 0;
   /** Whether the carousel bounds fold into each other. */
   @Input() revolves = true;
   /** Whether the carousel includes navigation arrows on either side of the carousel. */
   @Input() hasArrows = true;
+  /** Navigation directions to display when arrow navigation is enabled. */
+  @Input() arrowDirs: CardinalDirection[] = ['east', 'west'];
+
+  private index: number;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.items[this.initialIndex]) {
+      this.index = this.initialIndex;
+    } else {
+      this.index = 0;
+    }
+  }
+
+  /**
+   * Returns the currently selected item index.
+   */
+  getCurrentIndex(): number {
+    return this.index;
+  }
 }
